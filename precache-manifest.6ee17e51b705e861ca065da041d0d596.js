ae9444191904668ac62583e76c3a6f0be848f8b2
Michael Alvin <michael.alvin@stonybrook.edu>
2019-02-09 00:10:10 -0500

    Person Website v2

---
+++ precache-manifest.6ee17e51b705e861ca065da041d0d596.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9632e95cc5a1d70074cd404927c416d",
+    "url": "/static/media/headshot.c9632e95.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f00308ef2fa0b23ae360",
+    "url": "/static/js/main.f00308ef.chunk.js"
+  },
+  {
+    "revision": "292b4ab96b6bbe799064",
+    "url": "/static/js/1.292b4ab9.chunk.js"
+  },
+  {
+    "revision": "f00308ef2fa0b23ae360",
+    "url": "/static/css/main.4f2662b1.chunk.css"
+  },
+  {
+    "revision": "292b4ab96b6bbe799064",
+    "url": "/static/css/1.af0cae22.chunk.css"
+  },
+  {
+    "revision": "882d71d4b0aa143b15029b632a9d43c3",
+    "url": "/index.html"
+  }
+];
